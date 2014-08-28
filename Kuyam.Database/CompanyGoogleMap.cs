3c054bb4f62422c5e77b9ae3da1a72e9b4f17dd7
phuongtruong <phuongtruong@vinasource.com>
2014-08-28 15:07:26 +0700

    +add image

---
+++ Kuyam.Database/CompanyGoogleMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kuyam.Database
+{
+    public class CompanyGoogleMap
+    {
+        public int IndexId { get; set; }
+        public int ProfileID { get; set; }
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string IconMarker { get; set; }
+        public string Slug { get; set; }
+    }
+}
---
+++ Kuyam.WebUI/Models/BookKing/BookingPageListModel.cs
@@ -0,0 +1,25 @@
+﻿using Kuyam.Database;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kuyam.WebUI.Models.BookKing
+{
+    public class BookingPageListModel
+    {
+        public bool IsLogin { get; set; }
+        public int CategoryId { get; set; }
+        public List<Service> Categories { get; set; }
+        public string HtmlCategories { get; set; }
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public string SearchKey { get; set; }
+        public string Page { get; set; }
+        public List<CompanyGoogleMap> locations { get; set; }
+        public string MarkerData { get; set; }
+        public int TotalPages { get; set; }
+        public IPagedList<CompanyProfileSearch> PagedList { get; set; }
+    }
+}
